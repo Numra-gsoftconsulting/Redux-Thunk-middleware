0cfa6da82bb0f23139b84f8fcd4999b162286e3a
numrajaved321 <numrajaved321@gmail.com>
2022-08-15 17:33:47 +0500

    redux project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+import ProductListing from './components/ProductListing';
+import ProductDetail from './components/ProductDetails';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <Header />
+  
+   {/* <Header />
+   
+   <Route path="/" element={ProductListing} />
+   <Route path="/product/:productId" element={ProductDetail} />
+   <Route>404 Not Found</Route> */}
+
+<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProductListing />}>
+         
+          
+
+          <Route path="/" exact component={ProductListing} />
+        
+         
+        </Route>
+        
+        <Route path="/product/:productId" component={ProductDetail} />
+      </Routes>
+    </BrowserRouter>
+  
+   
+
+   
+     {/* <Header />
+     
+     <ProductListing />
+     <ProductDetail /> */}
+      {/* <BrowserRouter>
+    <Routes>
+
+   
+      
+      <Route path="/" element={ProductListing} />
+      <Route path="/product/:productId" element={ProductDetail} />
+     
+   
+  </Routes>
+</BrowserRouter> */}
+   
     </div>
   );
 }
